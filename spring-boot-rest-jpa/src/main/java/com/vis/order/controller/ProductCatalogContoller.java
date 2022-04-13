a57325085b6affee5a1ddc74e8d20958c28e94d9
vishwasraiborde <vishwasraiborde@vishwass-mbp>
2022-04-13 23:57:09 +0100

    test

--- spring-boot-rest-jpa/src/main/java/com/vis/order/controller/ProductCatalogContoller.java
+++ spring-boot-rest-jpa/src/main/java/com/vis/order/controller/ProductCatalogContoller.java
@@ -3,7 +3,10 @@ package com.vis.order.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.vis.order.data.ProductCategory;
@@ -17,10 +20,26 @@ public class ProductCatalogContoller {
 
 	@GetMapping("/categories/")
 	public List<ProductCategory> productCategory () {
-		
 		return productCategoryService.getProductCategories();
+	}
+	
+	@PostMapping("/category/")
+	public ProductCategory saveProductCategory (@RequestBody ProductCategory productCategory) {
+		return productCategoryService.save(productCategory);
+	}
+	
+	@DeleteMapping
+	@PostMapping("/category/")
+	public ProductCategory deleteProductCategory (@RequestBody ProductCategory productCategory) {
 		
-		
+		try {
+			return productCategoryService.delete(productCategory);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// this is bad, need to put a wrapper for exception and error conditions
+		return null;
 	}
 	
 }
--- spring-boot-rest-jpa/src/main/java/com/vis/order/service/ProductCategoryService.java
+++ spring-boot-rest-jpa/src/main/java/com/vis/order/service/ProductCategoryService.java
@@ -2,12 +2,15 @@ package com.vis.order.service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.vis.order.data.ProductCategory;
 import com.vis.order.repository.ProductCategoryRepo;
+
+import io.netty.util.internal.ObjectUtil;
 
 @Service
 public class ProductCategoryService {
@@ -20,6 +23,22 @@ public class ProductCategoryService {
 		List<ProductCategory> result = new ArrayList<ProductCategory>();    
 		 productCategoryRepo.findAll().forEach(result::add);
 		 return result;
+	}
+
+
+	public ProductCategory save(ProductCategory productCategory) {
+		return productCategoryRepo.save(productCategory);
+	}
+
+
+	public ProductCategory delete(ProductCategory productCategory) throws Exception {
+		
+		Optional.ofNullable(productCategory.getProductCode())
+		        .orElseThrow(() -> new Exception(""));
+		
+		
+		productCategoryRepo.delete(productCategory);
+		return productCategory;
 	}
 
 }
